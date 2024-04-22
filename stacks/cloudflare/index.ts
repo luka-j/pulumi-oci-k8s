109d5ddf4435e3ee744b36a8a5217889c8e1a002
Luka Jovicic <11800546+luka-j@users.noreply.github.com>
2024-04-23 00:54:54 +0200

    Add SSL off Page Rule to Cloudflare stack

--- stacks/cloudflare/index.ts
+++ stacks/cloudflare/index.ts
@@ -5,20 +5,30 @@ import * as pulumi_kubernetes from "@pulumi/kubernetes";
 
 const config = new Config();
 
+const domain = config.require("domain");
 const zones = cloudflare.getZonesOutput({
     filter: {
-        name: config.require("domain"),
+        name: domain,
         status: "active",
     },
 });
 
-
+const dnsRecordName = config.get("subdomain") ? `*.${config.get("subdomain")}` : "*";
 const dnsRecord = new cloudflare.Record("dns_record", {
     zoneId: zones.apply(zs => zs.zones[0].id!),
-    name: config.get("subdomain") ? `*.${config.get("subdomain")}` : "*",
+    name: dnsRecordName,
     value: config.require("ip"),
     type: "A",
     proxied: false,
+});
+
+const ruleSslOff = new cloudflare.PageRule("page_rule_ssl_off", {
+    zoneId: zones.apply(zs => zs.zones[0].id!),
+    target: `${dnsRecordName}.${domain}/*`,
+    priority: 1,
+    actions: {
+        ssl: "off",
+    },
 });
 
 if(config.get("annotateIngressWithCertManager")) {
@@ -26,20 +36,18 @@ if(config.get("annotateIngressWithCertManager")) {
         kubeconfig: config.require("kubeconfig"),
     });
 
-    all([dnsRecord.urn]).apply(() => { // await cf dns record creation before triggering cert manager
-        const ingressResourceName = config.require("annotateIngressWithCertManager");
-        const ingressNameParts = ingressResourceName.split("/", 2);
-        const ingressPatch = new IngressPatch("argo_ingress_patch", {
-            metadata: {
-                namespace: ingressNameParts[0],
-                name: ingressNameParts[1],
-                annotations: {
-                    "cert-manager.io/cluster-issuer": config.require("certManagerIssuer"),
-                    "pulumi.com/patchForce": "true",
-                }
-            },
-        }, { provider: kubernetesProvider });
-    });
+    const ingressResourceName = config.require("annotateIngressWithCertManager");
+    const ingressNameParts = ingressResourceName.split("/", 2);
+    const ingressPatch = new IngressPatch("argo_ingress_patch", {
+        metadata: {
+            namespace: ingressNameParts[0],
+            name: ingressNameParts[1],
+            annotations: {
+                "cert-manager.io/cluster-issuer": config.require("certManagerIssuer"),
+                "pulumi.com/patchForce": "true",
+            }
+        },
+    }, { provider: kubernetesProvider, dependsOn: dnsRecord });
 }
 
-export {dnsRecord}
+export {dnsRecord, ruleSslOff}
